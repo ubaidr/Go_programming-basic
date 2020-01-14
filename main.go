29045ad3d7d0e6824c2818aa34a96102c57f5c28
ObaidGO <ubaidr77@gmail.com>
2020-01-14 16:26:15 +0500

    change

--- main.go
+++ main.go
@@ -10,8 +10,16 @@ func main()  {
 	z:=abc(2,3)
 	fmt.Println(z)
 
+	x:=plus(1,2,3,4 )
+	fmt.Println("this is function return ",x)
+
+
 }
 
+func plus(a int ,b int,c int , d int )int{
+	return a+b+c+d
+
+}
 func obaid(a string) string {
 
 	fmt.Println("obaid uh are in func now")
